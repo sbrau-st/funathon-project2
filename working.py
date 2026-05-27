48bab5b74c9bc3ad962e55aba98eefa9d06537f1
sbrau <susanna.braunschweig@statistik.gv.at>
2026-05-27 14:46:45 +0000

    Train

--- working.py
+++ working.py
@@ -3,6 +3,11 @@ import mlflow
 from dotenv import load_dotenv
 import polars as pl
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from torchTextClassifiers.value_encoder import ValueEncoder
+from torchTextClassifiers.tokenizers import WordPieceTokenizer
+from torchTextClassifiers import ModelConfig, TrainingConfig, torchTextClassifiers
+
 
 load_dotenv(override=True)
 
@@ -26,4 +31,51 @@ X_test, y_test = test_df["label"].to_numpy(), test_df["code"].to_numpy()
 
 print(f"Train: {len(train_df)} | Val: {len(val_df)} | Test: {len(test_df)}")
 
+# %%
+encoder = LabelEncoder()
+encoder.fit(train_df['code'].to_numpy())
+
+all_codes = set(df['code'])
+train_codes = set(train_df['code'])
+missing = all_codes - train_codes
+
+if missing:
+    print(f"WARNING: {len(missing)} code(s) missing from training set: {missing}")
+else:
+    print(f"OK — all {len(all_codes)} codes appear in the training set.")
+# %%
+
+value_encoder = ValueEncoder(label_encoder=encoder)
+
+# %%
+tokenizer = WordPieceTokenizer(vocab_size=5000, output_dim=10)
+tokenizer.train(X_train)
+
+print("Output tensor size:", tokenizer.tokenize(X_train[0]).input_ids.shape)
+print("Vocabulary size:", tokenizer.vocab_size)
+
+# Look at an example of tokenization
+print("Raw text", X_train[0])
+print(
+    "Tokens id:",
+    tokenizer.tokenize(X_train[0]).input_ids.squeeze(0)
+)
+print(
+    "Tokens:",
+    tokenizer.tokenizer.convert_ids_to_tokens(
+        tokenizer.tokenize(X_train[0]).input_ids.squeeze(0)
+    )
+)
+# %%
+embedding_dim = 96
+
+model_config = ModelConfig(
+    embedding_dim=embedding_dim,
+    num_classes=n_classes,)
+
+ttc = torchTextClassifiers(
+    tokenizer=tokenizer,
+    model_config=model_config,
+    value_encoder=value_encoder,
+)
 # %%
